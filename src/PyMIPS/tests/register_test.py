946990b0fc832d2dd1a27c22ad9d6078af2ac164
AshtonUPS <awalden@pugetsound.edu>
2019-04-18 06:51:46 -0700

    Refactor

--- src/PyMIPS/tests/register_test.py
+++ src/PyMIPS/tests/register_test.py
@@ -4,6 +4,6 @@ from PyMIPS.Datastructure.register import *
 def test_setting_to_num():
     rp = RegisterPool.get_instance()
     t1 = rp.get_register("$t1")
-    assert t1.get_contents() == None
+    assert t1.get_contents() == 0
     t1.set_contents(12)
     assert t1.get_contents() == 12

b90ff7db3a28325d2e6fe689c9f6efd14e8b0da9
AshtonUPS <awalden@pugetsound.edu>
2019-04-29 23:09:32 -0700

    Dummy execution_stack

--- src/PyMIPS/Datastructure/execution_stack.py
+++ src/PyMIPS/Datastructure/execution_stack.py
@@ -1,4 +1,18 @@
-class ExecutionStack:
-    def __init__(self, ast):
-        self.ast = ast
+from PyMIPS.Datastructure.register import RegisterPool
+from PyMIPS.Datastructure.immediate import StoredRefs
+from PyMIPS.Datastructure.commands import data_stack
 
+rp = RegisterPool.get_instance()
+
+
+def run_from_list(commands: list):
+    iteration = 0
+    for c in commands:
+        print(f"({iteration}): {str(c)}")
+        c()
+        rp.print_all_active_registers()
+        print()
+        StoredRefs.print_all_active_refs()
+        print()
+        data_stack.print_stack()
+        iteration += 1

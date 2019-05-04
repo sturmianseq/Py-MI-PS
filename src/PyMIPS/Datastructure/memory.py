47772a778de6f73e7114fd0db84d1c441ce2a8d5
Ashton Walden <awalden@pugetsound.edu>
2019-05-04 15:19:32 -0700

    Finished Memory

--- src/PyMIPS/Datastructure/memory.py
+++ src/PyMIPS/Datastructure/memory.py
@@ -1,32 +1,85 @@
 class MemoryBlock:
-    def __init__(self, value, base_address, size):
+    def __init__(self, value, base_address:int, size:int):
+        """Handles maintaining memory blocks
+        
+        Parameters
+        ----------
+        value : Any
+            The value stored in the block
+        base_address : int
+            The beginning address of the data
+        size : int
+            The size of the data block
+        """
         self.value = value
         self.base_address = base_address
         self.size = size
 
-    def __call__(self, address):
+    def __call__(self, address:int):
+        """Returns the stored data given an address
+        
+        Parameters
+        ----------
+        address : int
+            The address to access
+        
+        Returns
+        -------
+        Any
+            The stored value
+        
+        Raises
+        ------
+        Exception
+            Bad Access
+        Exception
+            Memory Corrupted
+        """
         if address != self.base_address:
             raise Exception(f"Bad Access: {address}")
 
         for i in range(1, self.size):
             new_val = Memory.__data[address + i]
-            if new_val
+            if new_val.base_address != self.base_address:
+                raise Exception(f"Memory Corrupted: {address}")
+
+        return self.value
+
 
 class Memory:
     __data = {}
 
     @staticmethod
     def store_value(value, address: int, size=4):
+    """Store a value in static memory
+    
+    Parameters
+    ----------
+    value : Any
+        A value to store in memory
+    address : int
+        The address to store the value
+    size : int, optional
+        The size in bytes of the value, by default 4
+    """
         for i in range(size):
             Memory.__data[address + i]
 
     @staticmethod
     def get_value(address: int):
+    """Retrives a value from memory
+    
+    Parameters
+    ----------
+    address : int
+        The address to access
+    
+    Returns
+    -------
+    Any
+        The value stored
+    """
         if address not in Memory.__data.keys():
             return 0
 
-        value = Memory.__data[address]
-        if value.base_address == address:
-            return value.value
-        else:
-            raise Exception(f"Bad Access: {address}")
+        return Memory.__data[address]()

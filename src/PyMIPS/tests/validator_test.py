49e9be91fada1964bfa138e7845e0c6222c093c9
Jiman Kim <jimankim@pugetsound.edu>
2019-05-07 11:10:23 -0700

    More tests

--- src/PyMIPS/tests/validator_test.py
+++ src/PyMIPS/tests/validator_test.py
@@ -3,10 +3,16 @@ from PyMIPS.AST.validator import validate
 import unittest
 
 
-def test_correct_rtype():
+def test_correct_3rtype():
     rtype_test = RType("add", "$t4", "$t2", "$t3")
     assert validate(rtype_test) == True
 
-def test_incorrect_rtype():
+
+def test_incorrect_3rtype():
     rtype_test = RType("li", "$t4", "$t2", "$t3")
     assert validate(rtype_test) == False
+
+
+def test_correct_2rtype():
+    r_test = RType("div", "$t4", "$t2")
+    assert validate(r_test) == True

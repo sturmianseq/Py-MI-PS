a020be5425ce34e385d00f4a6c078dd0b8c13f1b
AshtonUPS <awalden@pugetsound.edu>
2019-05-09 02:52:27 -0700

    Run program

--- src/PyMIPS/Datastructure/execution_stack.py
+++ src/PyMIPS/Datastructure/execution_stack.py
@@ -1,5 +1,11 @@
-from PyMIPS.Datastructure.data_model import RegisterPool, DataStack, DataHeap
+from PyMIPS.Datastructure.data_model import (
+    ProgramStack,
+    RegisterPool,
+    DataStack,
+    DataHeap,
+)
 from PyMIPS.Datastructure.memory import Memory
+from PyMIPS.AST.blocks import Program
 
 
 def run_from_list(commands: list):
@@ -12,3 +18,29 @@ def run_from_list(commands: list):
         iteration += 1
     return True
 
+
+def run_program(p: Program):
+    # Reset everything
+    Memory.reset()
+    ProgramStack.reset
+    DataHeap.reset()
+
+    # Allocate data block
+    p.data()
+
+    # Push the instructions to program stack
+    ProgramStack.add_text_block(p.text)
+
+    # Set the program counter
+    p.globl()
+
+    # Run the program
+    print(p)
+    print("----------------------\nSTART OF PROGRAM")
+    while True:
+        try:
+            ProgramStack.execute_next()
+            # RegisterPool.print_all_active_registers()
+        except Exception as e:
+            print(e.with_traceback())
+            break

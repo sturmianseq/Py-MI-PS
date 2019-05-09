284c3d4d2a75b2cca7abed6395956b340d63e118
Jiman Kim <jimankim@pugetsound.edu>
2019-05-08 21:05:47 -0700

    Added unit testing for validation

--- src/PyMIPS/tests/validator_test.py
+++ src/PyMIPS/tests/validator_test.py
@@ -3,16 +3,42 @@ from PyMIPS.AST.validator import validate
 import unittest
 
 
-def test_correct_3rtype():
-    rtype_test = RType("add", "$t4", "$t2", "$t3")
-    assert validate(rtype_test) == True
+class TestRType(unittest.TestCase):
+    def test_3(self):
+        try:
+            RType("add", "$t3", "$t2", "$t1")
+        except:
+            self.fail()
+
+    def test_bad_3(self):
+        with self.assertRaises(Exception):
+            RType("add", "$t3")
 
 
-def test_incorrect_3rtype():
-    rtype_test = RType("li", "$t4", "$t2", "$t3")
-    assert validate(rtype_test) == False
+class TestIType(unittest.TestCase):
+    def test_3(self):
+        try:
+            IType("lw", "$t0", 4, "$sp")
+        except:
+            self.fail()
 
+    def test_bad_3(self):
+        with self.assertRaises(Exception):
+            IType("lw", "$t0", immediate=None)
+        with self.assertRaises(Exception):
+            IType("add", "$t32")
+        with self.assertRaises(Exception):
+            IType("lw", "t32", immediate=None)
 
-def test_correct_2rtype():
-    r_test = RType("div", "$t4", "$t2")
-    assert validate(r_test) == True
+    def test_2(self):
+        try:
+            IType("li", "$t0", 4)
+            IType("sw", "$t0", -4, "$sp")
+        except:
+            self.fail()
+
+    def test_bad_2(self):
+        with self.assertRaises(Exception):
+            IType("li", "$t3", source="$t2")
+        with self.assertRaises(Exception):
+            IType("li", source="$t2")

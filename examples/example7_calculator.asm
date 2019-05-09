2ae0bc353b39dc9d0e06d266ad3250cb470b5f96
AshtonUPS <awalden@pugetsound.edu>
2019-05-09 02:51:48 -0700

    Using my calculator

--- examples/example7_calculator.asm
+++ examples/example7_calculator.asm
@@ -1,233 +1,192 @@
-	.data
-	intPrompt:	.asciiz "\nPlease enter an integer: "
-	opPrompt:	.asciiz "Please enter an operation (+,-,*,/): " 
-	oflow: 		.asciiz "I'm sorry that would overflow."
-	divide0:	.asciiz "I'm sorry, you cannot divide by 0."
-	result:		.asciiz "Thank you. "
-	remainder:	.asciiz " r "
-	space:		.asciiz " = "
-	a1 : 		.word 0
-    	a2 : 		.word 0
-    	op:  		.byte '-'
-#-------------------------
-#------Calculator App-----
-#---Author: Jiman Kim-----
-#-------------------------	
-	.text
-main:	addi 	$sp,$sp,-16
-	sw	$a0, 0 ($sp)
-	sw	$s0, 4 ($sp)
-	sw	$s1, 8 ($sp)
-	sw	$ra, 12 ($sp)
+.data
+Question1:
+	.asciiz	"Please enter an integer: "
+Question2:
+	.asciiz	"Please enter an operation (+,-,*,/): "
+Right:
+	.asciiz	"Thank you. "
+Overflow:
+	.asciiz	"I'm sorry, that would overflow."
+Div_By_0:
+	.asciiz "I'm sorry, you can't divide by zero."
 
-	#Prints 1st int
-	li	$v0, 4
-	la	$a0, intPrompt
-	syscall 
-	
-	#Reads 1st int
-	li	$v0, 5
-	syscall	
-	sw 	$v0 a1
-	
-	#Prints Operator
-	li 	$v0, 4
-	la	$a0, opPrompt
-	syscall
-	
-	#Reads Operator
-	li 	$v0,12
-	syscall
-	sw 	$v0 op
-	
-	#Prints 2nd int
-	li 	$v0, 4
-    	la 	$a0, intPrompt
-    	syscall
+Buffer:
+	.space 4
 
-	#Reads 2nd int
-    	li 	$v0,5        
-        syscall
-    	sw 	$v0 a2
+.text
+main:	
+	la 	$a0, Question1	#load the address Question and store it into $a0
+	jal 	display_question
+	jal	get_int
+	move	$s0, $v0	#Store the first operand in $s0
 	
-	#Loads in the ints and operator
-    	lw 	$s0 a1
-     	lw	$s1 a2
-    	lw	$s2 op
+	la	$a0, Question2
+	jal	display_question
+	jal	get_operator
 	
-	#Checks to see which operator to do
-	li	$t3, '+'
-	li	$t4, '-'
-	li	$t5, '*'
-	li	$t6, '/'
-	#Calls the correct math function
-	beq	$s2, $t3, addition   
-	beq	$s2, $t4, subtraction 
-	beq	$s2, $t5, multiplication 
-	beq	$s2, $t6, division  	
+	lb	$s2, 0($a0)	#Operator in $s2
+	
+	la	$a0, Question1
+	jal	display_question
+	jal	get_int
+	move	$s1, $v0	#Store the second operand in $s1
+	
+	move	$a0, $s0
+	move	$a1, $s2
+	move	$a2, $s1
+	li	$v1, 0
+	jal	operate
 
-#-------------------------
-#------Prints function----
-#-------------------------	
-print:
-	#Prints Thank you message
-	li 	$v0 4
-    	la 	$a0 result
-    	syscall 
-    	#Prints first int
-    	li 	$v0 1
-    	move 	$a0 $s0
-    	syscall
-    	#Prints operator
-        li 	$v0 11
-    	move 	$a0 $s2
-    	syscall
-    	#Prints second int
-        li 	$v0 1
-    	move 	$a0 $s1
-    	syscall
-    	#Prints space
-    	li 	$v0 4
-    	la 	$a0 space
-    	syscall 
-    	jr 	$ra	
-    	
-exitCode:
-	#Puts registers back 
-	lw	$a0, 0 ($sp)
-	lw	$s0, 4 ($sp)
-	lw	$s1, 8 ($sp)
-	lw	$ra, 12 ($sp)
-	#Exits code
-	li 	$v0, 10
-	syscall	
+	move	$a0, $s0
+	move	$a1, $s2
+	move	$a2, $s1
+	move	$a3, $v0
+	jal	display_end
+
+	jal	exit		#Exit
+	
+operate:	#$v0 will be the result if it succeeds. System will end if not.
+#Args: $a0 is first operand, $a1 is operator, $a2 is second operand
+	li      $t0, 43
+	beq 	$a1, $t0, plus
+	li      $t0, 45
+	beq 	$a1, $t0, minus
+	li      $t0, 42
+	beq 	$a1, $t0, multiply
+	li      $t0, 47
+	beq 	$a1, $t0, divide
+	jal	exit	#If all fails, exit
+	
+plus:
+	addu  	$v0, $a0, $a2	#Add the two
+	#Only time there is overflow is when both operators have a different sign from the result
+	xor	$t0, $a0, $v0	#High bit will be 1 if different
+	xor	$t1, $a2, $v0	#High bit will be 1 if different
+	and	$t0, $t0, $t1	#High bit will be 1 if both are different
+	bltz 	$t0, overflow	#If high bit is 1, then the number is less than 0. Therefore overflow
+	jr	$ra		#Else return
+
+minus:
+	subu	$v0, $a0, $a2
+	#Only time there is an overflow is when one operator is a different sign from the result
+	not	$t1, $a2	#Flip the second operator (pos - neg = pos) to make them the same
+	xor	$t0, $a0, $v0	#High bit will be 1 if different
+	xor	$t1, $t1, $v0	#High bit will be 1 if different
+	and	$t0, $t0, $t1	#High bit will be 1 if both are different
+	bltz 	$t0, overflow	#If high bit is 1, then the number is less than 0. Therefore overflow
+	jr	$ra		#Else Return
+
+
+multiply:
+	mult 	$a0, $a2	#Multiply the two
+	mfhi	$t0		#Get the high
+	mflo	$v0		#Get the result
+	li		$t1, 4294967295
+	beq	$t0, $t1,multiply_cont
+	beq	$t0, $zero,multiply_cont
+	j	overflow	#If the high bits are not equal to 0xffffffff or 0x00000000 then it has overflown
+multiply_cont:
+	xor	$t0, $v0, $t0	#See if these have different signs
+	bltz	$t0, overflow
+	jr	$ra
+	
+divide:
+	beqz	$a2, divide_by_zero		#Divide by zero
+	li      $t3, 0x80000000
+	bne 	$a0, $t3, no_div_overflow #If a0 isn't the largest signed number, we can't have an overflow
+	li      $t3, -1
+	beq 	$a2, $t3, overflow		#If a0 is the largest signed number and a2 is -1, we overflow
+	
+no_div_overflow:
+	div	$a0, $a2
+	mflo	$v0
+	mfhi	$v1
+	jr	$ra
+	
+divide_by_zero:
+	la	$a0, Div_By_0
+	jal	display_question
+	jal	exit
 	
 overflow:
-	#Prints overflow message
+	la	$a0, Overflow
+	jal	display_question
+	jal	exit
+	
+display_question:		#Mutates $v0
 	li	$v0, 4
-	la	$a0 oflow
 	syscall
-	j 	exitCode
-#-------------------------
-#------Add function-------
-#-------------------------
-addition:
-	#Adds and checks if signs are the same and branch to either
-	#possible overflow or no overflow
-	addu  	$t2, $s0, $s1
-	xor	$t3, $s0, $s1
-	bgez 	$t3, add_overflow
-	blez 	$t3, add_no_overflow
+	jr	$ra
 
-	
-add_no_overflow:
-	#Prints the sum
-	jal 	print
-	li 	$v0 1
-	move $a0, $t2	
+get_int:			#Result is in $v0
+	li	$v0, 5
 	syscall
-	j exitCode
+	jr	$ra
 	
-add_overflow:
-	#Check if sum overflowed or not
-	xor	$t0, $t2, $s0
-	blez	$t0, overflow
-	bgez	$t0, add_no_overflow
-	
-#-------------------------
-#------Sub function-------
-#-------------------------	
-subtraction:
-	#Checks if b > a, then possible overflow
-	#Or no overflow possible
-	subu	$t2, $s0, $s1
-	bgt	$s1, $s0, sub_overflow 
-	blt	$s1, $s0, sub_no_overflow
-
-sub_no_overflow:
-	#Prints the difference
-	jal print
+print_int:
 	li	$v0, 1
-	move 	$a0, $t2
 	syscall
-	j exitCode
+	jr	$ra
 	
-sub_overflow:
-	#Checks difference sign with sign of a to catch overflow
-	xor	$t0, $t2, $s0
-	bltz	$t0, overflow
-	bgez	$t0, sub_no_overflow
+print_char:
+	li	$v0, 11
+	syscall
+	jr	$ra
+
+get_operator:			#Result pointer in $a0
+	li	$v0, 8		#code for reading a string
+	la	$a0, Buffer	#Load input buffer address
+	li	$a1, 4		#Set maximum chars to 4 (Character and null terminator)
+	syscall
+	jr	$ra
 	
-#-------------------------
-#------Multiply function--
-#-------------------------
-multiplication:
-     	# multiplies ints and stores the lower 32 bits	
-	multu $s0, $s1
-	mflo $t5
-	#Checks if signs are the same 
-	xor $t0, $s0, $s1 
-	bgez $t0, multi_pos
-	#Else, checks for a negative input
-	bltz $s0, multi_neg
-	bltz $s1, multi_neg
-
-multi_neg:
-	#If there is a negative input
-	#Overflow if product is positive
-	slt $t0, $t5, $zero
-	beqz $t0, overflow	
-	#Else, print product
-	jal print
-	li $v0, 1
-	move $a0, $t5
-	syscall
-	j exitCode
-
+compare:			#v0 is 1 if true, 0 if false
+	li	$v0, 0
+	beq 	$a0, $a1, compare_equal
+	jr	$ra
+compare_equal:
+	li	$v0, 1
+	jr	$ra
 	
-multi_pos:
-	#If signs are the same, check if product is negative
-	#If so, overflow, else print product
-	slt  $t0, $t5, $zero
-	bgtz $t0, overflow  
-	jal print
-	li $v0, 1
-	move $a0, $t5
-	syscall
-	j exitCode
-#-------------------------
-#------Divide function----
-#-------------------------
-division:
-	#Base case variables
-	li	$t4, 0x80000000
-	li 	$v0 1
-	#Check divide by 0 case
-	beqz	$s1, div_by_zero
-	#Check single overflow case
-	seq     $t1, $s0, $t4
-	seq	$t2, $s1, -1
-	and 	$t3, $t1, $t2
-	beq     $t3, 1, overflow 
-	#Divdes and prints quotient and remainder
-	div  	$s0, $s1 
-	mflo	$t0
-	mfhi	$t1
-	jal print
-	li $v0, 1
-	move $a0, $t0
-	syscall
-	li $v0, 4
-	la $a0, remainder
-	syscall
-	li $v0, 1
-	move $a0, $t1
-	syscall
-	j exitCode
-
-div_by_zero:
-	#Call divide by zero error string
-	li $v0, 4
-	la $a0, divide0
-	syscall
-	j exitCode
+display_end:
+	move	$t0, $a0	#Store $a0 for later
+	move	$t1, $a1
+	move	$t2, $a2
+	move	$t3, $a3
+	move	$t4, $ra
+	la	$a0, Right
+	jal	display_question
+	move	$a0, $t0
+	jal	print_int	#Print the first int
+	li	$a0,	32	#Space
+	jal	print_char
+	move	$a0, $t1	#Print the operator
+	jal	print_char
+	li	$a0,	32	#Space
+	jal	print_char
+	move	$a0, $t2
+	jal	print_int	#Print the second int
+	li	$a0,	32	#Space
+	jal	print_char
+	li	$a0,	61	#Space
+	jal	print_char
+	li	$a0,	32	#Space
+	jal	print_char
+	move	$a0, $t3
+	jal	print_int	#Print the answer
+	bnez 	$v1, remainder
+	jr	$t4
+remainder:
+	li	$a0,	32	#Space
+	jal	print_char
+	li	$a0,	114	#r
+	jal	print_char
+	li	$a0,	32	#Space
+	jal	print_char
+	move	$a0, $v1
+	jal	print_int	#Print the remainder
+	jr	$t4
+	
+exit:
+	li	$v0, 10
+	syscall			#Exit cleanly

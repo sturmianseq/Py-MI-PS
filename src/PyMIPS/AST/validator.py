2b44ce435321ff719b8fbfc91883db5bd12023ce
Jiman Kim <jimankim@pugetsound.edu>
2019-05-08 21:04:47 -0700

    Added validate for opt 2 reg instructions

--- src/PyMIPS/AST/validator.py
+++ src/PyMIPS/AST/validator.py
@@ -12,8 +12,9 @@ def validate(instruction) -> bool:
         Valid or not valid
     """
     switch = {
-        "li": validate_li,
-        "sw": validate_sw,
+        "lw": validate_optional_2_type,
+        "li": validate_1_itype,
+        "sw": validate_optional_2_type,
         "add": validate_3_rtype,
         "sub": validate_3_rtype,
         "div": validate_2_rtype,
@@ -63,21 +64,47 @@ def validate_1_rtype(instruction) -> bool:
     return check1 and check2 and check3
 
 
-def validate_li(instruction) -> bool:
-    target = instruction.target_register
+def validate_2_itype(instruction) -> bool:
+    destination = instruction.destination_register
     source = instruction.source_register
+    target = instruction.target_register
     immediate = instruction.immediate
 
-    check1 = target is not None
-    check2 = source is None
+    check1 = destination is not None
+    check2 = source is not None
     check3 = immediate is not None
+    check4 = target is None
+    print(check1, check2, check3, check4)
 
-    return check1 and check2 and check3
-
-
-def validate_sw(instruction):
-    return True
+    return check1 and check2 and check3 and check4
 
 
-def validate_add(instruction):
-    return True
+def validate_optional_2_type(instruction) -> bool:
+    destination = instruction.destination_register
+    source = instruction.source_register
+    target = instruction.target_register
+    immediate = instruction.immediate
+
+    check1 = destination is not None
+    check2 = source is not None or None
+    check3 = immediate is not None
+    check4 = target is None
+    print(check1, check2, check3, check4)
+
+    return check1 and check2 and check3 and check4
+
+
+def validate_1_itype(instruction) -> bool:
+    destination = instruction.destination_register
+    target = instruction.target_register
+    immediate = instruction.immediate
+    source = instruction.source_register
+
+    check1 = destination is not None
+    check2 = target is None
+    check3 = immediate is not None
+    check4 = source is None
+    print(check1, check2, check3, check4)
+
+    return check1 and check2 and check3 and check4
+

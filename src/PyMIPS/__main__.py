209ea14d4d661344fc70e860b6180260699854e2
AshtonUPS <awalden@pugetsound.edu>
2019-05-09 20:15:44 -0700

    Very basic cli

--- src/PyMIPS/__main__.py
+++ src/PyMIPS/__main__.py
@@ -7,7 +7,19 @@ from PyMIPS.Datastructure.execution_stack import run_program
 
 
 def main(args=None):
-    print("Cool")
+    if len(sys.argv) == 1:
+        print("Usage: pymips <path to file>")
+        return
+    elif len(sys.argv) > 2:
+        print("Too many arguments")
+        return
+    else:
+        filename = sys.argv[1]
+        with open(filename) as file:
+            characters = file.read()
+        tokens = lex(characters)
+        res = parse(tokens)
+        run_program(res.value)
 
 
 if __name__ == "__main__":

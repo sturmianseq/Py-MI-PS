6fbb6adabf206f37bbe666011cbef58f6fe067ad
AshtonUPS <awalden@pugetsound.edu>
2019-05-09 20:11:22 -0700

    Setup

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from setuptools import setup
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
 
 setup(
     name="PyMIPS",
+    license="MIT",
     version="1.0",
     description="A python MIPS interpreter",
     author="Ashton Walden & Jiman Kim",
     author_email="awalden@pugetsound.edu",
-    packages=["PyMIPS"],
-    package_dir={"PyMIPS": "src/PyMIPS"},
+    package_dir={"": "src"},
+    packages=find_packages(where="src", exclude=["tests"]),
+    include_package_data=True,
+    entry_points={"console_scripts": ["pymips = PyMIPS.__main__:main"]},
 )
---
+++ src/PyMIPS/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python
+import sys
+
+from PyMIPS.lexer import lex
+from PyMIPS.AST.ast import parse
+from PyMIPS.Datastructure.execution_stack import run_program
+
+
+def main(args=None):
+    print("Cool")
+
+
+if __name__ == "__main__":
+    main()

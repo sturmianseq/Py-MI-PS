2e8002a04ff9c5f266ef278d14398ac06c79b96c
AshtonUPS <awalden@pugetsound.edu>
2019-05-10 00:52:22 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 
 setup(
     name="PyMIPS",
     license="MIT",
-    version="1.0",
+    version="1.0.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AshtonUPS/Py-MI-PS",
     description="A python MIPS interpreter",
     author="Ashton Walden & Jiman Kim",
     author_email="awalden@pugetsound.edu",

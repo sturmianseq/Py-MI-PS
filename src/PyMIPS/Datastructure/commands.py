f49de3cb55df294156bf474b9b7c5f6c426924b9
AshtonUPS <awalden@pugetsound.edu>
2019-05-13 19:01:55 -0700

    implemented srl

--- src/PyMIPS/Datastructure/commands.py
+++ src/PyMIPS/Datastructure/commands.py
@@ -43,7 +43,7 @@ def get_command(ast_class):
             "or": or_command,
             # Unimplemented r-types
             "nor": unimplemented,
-            "srl": unimplemented,
+            "srl": srl_command,
             "srlv": unimplemented,
             "sllv": unimplemented,
             "sll": unimplemented,
@@ -96,6 +96,18 @@ def not_command(command):
         source = command.source_register.get_contents_as_int()
         res = ~source
         destination.set_contents_from_int(res)
+
+    return exe
+
+
+def srl_command(command):
+    def exe():
+        amount = command.immediate()
+        source = command.source_register.get_contents_as_unsigned_int()
+        dest = command.destination_register
+
+        res = source >> amount
+        dest.set_contents_from_int(res)
 
     return exe
 
